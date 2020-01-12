d30aafe88ce7bc85ee24c72ec91a41851ec1b354
bezamo <bezademamu@gmail.com>
2020-01-12 20:17:16 +0300

    Added comment_handler.go to delivery/http/handler folder and updated main.go file in the delivery/http/api folder

--- delivery/http/api/main.go
+++ delivery/http/api/main.go
@@ -9,9 +9,20 @@ import (
 	"github.com/rs/cors"
 
 	"github.com/hellyab/techreview/delivery/http/handler"
-	"github.com/hellyab/techreview/question/repository"
-	"github.com/hellyab/techreview/question/service"
+
+	ansRepo "github.com/hellyab/techreview/answer/repository"
+	ansServ "github.com/hellyab/techreview/answer/service"
+
+	questRepo "github.com/hellyab/techreview/question/repository"
+	questServ "github.com/hellyab/techreview/question/service"
+
+	commRepo "github.com/hellyab/techreview/comment/repository"
+	commServ "github.com/hellyab/techreview/comment/service"
 )
+
+//roleRepo
+//roleSrv
+//some role handler
 
 func main() {
 	dbconn, err := gorm.Open("postgres", "postgres://postgres:password@localhost/tech_review_test?sslmode=disable")
@@ -22,13 +33,17 @@ func main() {
 
 	defer dbconn.Close()
 
-	//roleRepo
-	//roleSrv
-	//some role handler
-
-	questionRepo := repository.NewQuestionGormRepo(dbconn)
-	questionSrv := service.NewQuestionService(questionRepo)
+	questionRepo := questRepo.NewQuestionGormRepo(dbconn)
+	questionSrv := questServ.NewQuestionService(questionRepo)
 	questionHandler := handler.NewQuestionHandler(questionSrv)
+
+	answerRepo := ansRepo.NewAnswerGormRepo(dbconn)
+	answerSrv := ansServ.NewAnswerService(answerRepo)
+	answerHandler := handler.NewAnswerHandler(answerSrv)
+
+	commentRepo := commRepo.NewCommentGormRepo(dbconn)
+	commentSrv := commServ.NewCommentService(commentRepo)
+	commentHandler := handler.NewCommentHandler(commentSrv)
 
 	router := httprouter.New()
 
@@ -38,10 +53,23 @@ func main() {
 	router.PUT("/questions/:id", questionHandler.PutQuestion)
 	router.DELETE("/questions/:id", questionHandler.DeleteQuestion)
 
-	handler := cors.New(cors.Options{
+	router.GET("/answers", answerHandler.GetAnswers)
+	router.GET("/answers/:id", answerHandler.GetAnswer)
+	router.POST("/answer", answerHandler.PostAnswer)
+	router.PUT("/answers/:id", answerHandler.PutAnswer)
+	router.DELETE("/answers/:id", answerHandler.DeleteAnswer)
+
+	router.GET("/comments", commentHandler.GetComments)
+	router.GET("/comments/:id", commentHandler.GetComment)
+	router.POST("/comment", commentHandler.UpdateComment)
+	router.DELETE("/comments/:id", commentHandler.DeleteComment)
+	router.PUT("/comments/:id", commentHandler.PutComment)
+
+	apiHandler := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
 		AllowedMethods: []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
 	}).Handler(router)
-	http.ListenAndServe(":8181", handler)
+
+	http.ListenAndServe(":8181", apiHandler)
 
 }
---
+++ delivery/http/handler/comment_handler.go
@@ -0,0 +1,182 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/hellyab/techreview/comment"
+	"github.com/hellyab/techreview/entities"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type CommentHandler struct {
+	commentService comment.CommentService
+}
+
+func NewCommentHandler(cmtService comment.CommentService) *CommentHandler {
+	return &CommentHandler{commentService: cmtService}
+}
+
+func (ch *CommentHandler) GetComments(w http.ResponseWriter,
+	r *http.Request, _ httprouter.Params) {
+
+	comments, errs := ch.commentService.Comments()
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(comments, "", "\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+
+	return
+}
+
+func (ch *CommentHandler) GetComment(w http.ResponseWriter,
+	r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+
+	// get the response (form "repo" -> "service" -> "handler" )
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	comments, errs := ch.commentService.Comment(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(comments, "", "\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+
+	return
+
+}
+
+func (ch *CommentHandler) UpdateComment(w http.ResponseWriter,
+	r *http.Request, _ httprouter.Params) {
+	l := r.ContentLength
+	body := make([]byte, l)
+	r.Body.Read(body)
+	comment := &entities.Comment{}
+	// sampleComment := &entities.Comment{
+	// 	ID:          1,
+	// 	CommentBody: "changed",
+	// 	UserName:    "test user name",
+	// }
+	err := json.Unmarshal(body, comment)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	comment, errs := ch.commentService.UpdateComment(comment)
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	p := fmt.Sprintf("/tech/comments/update/%d", comment.ID)
+	w.Header().Set("Location", p)
+	w.WriteHeader(http.StatusCreated)
+	return
+}
+
+func (ch *CommentHandler) DeleteComment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	_, errs := ch.commentService.DeleteComment(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+	return
+}
+func (ch *CommentHandler) PutComment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	comment, errs := ch.commentService.Comment(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	l := r.ContentLength
+
+	body := make([]byte, l)
+
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &comment)
+
+	comment, errs = ch.commentService.UpdateComment(comment)
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(comment, "", "\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
