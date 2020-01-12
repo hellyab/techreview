2e914316da7ec016b78e5919e6b3d5f4ff1cc996
GrimHaze <selamsileshi57@gmail.com>
2020-01-12 20:36:10 +0300

    Added answer_service.go to answer/service, gorm_repository.go to answer/repository and updated main.go file in the delivery/http/api folder

---
+++ answer/service/answer_service.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"github.com/hellyab/techreview/answer"
+	"github.com/hellyab/techreview/entities"
+)
+
+//AnswerService implements answer.Service
+type AnswerService struct {
+	answerRepo answer.AnswerRepository
+}
+
+// NewAnswerService returns a new AnswerService object
+func NewAnswerService(ansRepo answer.AnswerRepository) answer.AnswerService {
+	return &AnswerService{answerRepo: ansRepo}
+}
+
+//Answers returns all stored Answers
+func (as *AnswerService) Answers() ([]entities.Answer, []error) {
+	qstns, errs := as.answerRepo.Answers()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return qstns, errs
+}
+
+//Answer retrieves stored answer given its id
+func (as *AnswerService) Answer(id string) (*entities.Answer, []error) {
+	qstn, errs := as.answerRepo.Answer(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return qstn, errs
+}
+
+//UpdateAnswer updates a given answer
+func (as *AnswerService) UpdateAnswer(answer *entities.Answer) (*entities.Answer, []error) {
+	qstn, errs := as.answerRepo.UpdateAnswer(answer)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return qstn, errs
+}
+
+//DeleteAnswer deletes a given answer
+func (as *AnswerService) DeleteAnswer(id string) (*entities.Answer, []error) {
+	qstn, errs := as.answerRepo.DeleteAnswer(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return qstn, errs
+}
+
+//StoreAnswer stores a given answer
+func (as *AnswerService) StoreAnswer(answer *entities.Answer) (*entities.Answer, []error) {
+	qstn, errs := as.answerRepo.StoreAnswer(answer)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return qstn, errs
+}
+
+//QuestionAnswers returns answers for a question
+func (as *AnswerService) QuestionAnswers(question *entities.Question, answer *entities.Answer) ([]entities.Answer, []error) {
+	qstns, errs := as.answerRepo.QuestionAnswers(question, answer)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return qstns, errs
+}
